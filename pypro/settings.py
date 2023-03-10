ed0ba0d6f8248b9fd3c4804b1288dc53ae7cc5e1
Ryan Araujo <ryanaraujo01112001@gmail.com>
2023-03-10 14:43:12 -0300

    ##

--- pypro/settings.py
+++ pypro/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['https://projeto-pypro.fly.dev/']
+CSRF_TRUSTED_ORIGINS = ['https://projeto-pypro.fly.dev']
